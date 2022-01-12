86cfc06dfe9f3582cf4c1f5d17953103d505f882
Brisset <corcinobrisset@gmail.com>
2022-01-11 22:52:00 -0500

    cambio en el post fecha y actualizar pagina

--- js/medium.js
+++ js/medium.js
@@ -4,15 +4,11 @@ $(document).ready(function(){
   const newPost = (newObj) => {
       $.post('https://medium-7cfcc-default-rtdb.firebaseio.com/.json', JSON.stringify(newObj), function(data) {
               console.log(data)
-              getPost()
               document.getElementById('closeModal').click()
       }).fail( function (err) {
           console.log(err)
       })
   }
-  let fecha = new Date()
-  let dateCreated = `${fecha.getDate()}/${fecha.getMonth() +1}/${fecha.getFullYear()}`
-  console.log(dateCreated )
 
   let sendPost = document.querySelector('#sendPost')
 
@@ -21,18 +17,19 @@ $(document).ready(function(){
       let title = document.querySelector('#title').value
       let imgPerfil = document.querySelector('#imgPerfil').value
       let author = document.querySelector('#autor').value
-      let date = fecha.getMonth()
+      let fecha = new Date()
+      let dateCreated = `${fecha.getDate()}/${fecha.getMonth() +1}/${fecha.getFullYear()}`
       let readingTime = document.querySelector('#readingTime').value
       let formFile = document.querySelector('#formFile').value
       let abstract = document.querySelector('#abstract').value
       let category = document.querySelector("#inputGroupSelect01").value;
       let postContent = document.querySelector('#postContent').value
-      console.log(title, imgPerfil, author, date, readingTime, formFile, abstract, category, postContent)
+      console.log(title, imgPerfil, author, dateCreated, readingTime, formFile, abstract, category, postContent)
       if(
           title !== '' &&
           imgPerfil !== '' &&
           author !== '' &&  
-          date !== '' &&
+          dateCreated !== '' &&
           readingTime !== '' &&
           formFile !== '' &&
           abstract !== '' &&
@@ -43,7 +40,7 @@ $(document).ready(function(){
             title: title,
             imgPerfil: imgPerfil,
             author: author,
-            date: date,
+            dateCreated: dateCreated,
             readingTime: readingTime,
             formFile: formFile,
             abstract: abstract,
@@ -51,15 +48,13 @@ $(document).ready(function(){
             category: category,
           }
           newPost(objNewPost)
-          //location.reload()
+          location.reload()
       } else {
           alert('Algunos datos estan vacios')
       }
 
   })
-
   /**------------ get jq -------------------- */
-
       $.ajax({
           method: 'GET',
           url: "https://medium-7cfcc-default-rtdb.firebaseio.com/.json"
@@ -67,7 +62,7 @@ $(document).ready(function(){
           console.log(data)
           let temp = ''
           for(post in data) {
-              let { title, imgPerfil, author, date, readingTime, formFile, abstract, postContent } = data[post]
+              let { title, imgPerfil, author, dateCreated, readingTime, formFile, abstract, postContent } = data[post]
               temp += `
               <div class="row g-0">
                 <div class="col-md-8 ">
@@ -80,7 +75,7 @@ $(document).ready(function(){
                     <a href="http://127.0.0.1:5503/interiorPost.html"></a>
                     <p class="card-text">${abstract}</p>
                       <div class="d-flex ">
-                      <p class="card-text"><small class="text-muted">${readingTime} ${'min read'} ${date}</small></p>
+                      <p class="card-text"><small class="text-muted">${readingTime} ${'min read'} ${dateCreated}</small></p>
                         <div class="col d-flex  justify-content-end">
                         <svg width="25" height="25" viewBox="0 0 25 25" fill="none" class="oz"><path d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929"></path></svg>
                         <svg class="overflow-dots-filled-25px_svg__svgIcon-use" width="25" height="25"><path d="M5 12.5c0 .55.2 1.02.59 1.41.39.4.86.59 1.41.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41A1.93 1.93 0 0 0 7 10.5c-.55 0-1.02.2-1.41.59-.4.39-.59.86-.59 1.41zm5.62 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.42.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41a1.93 1.93 0 0 0-1.41-.59c-.55 0-1.03.2-1.42.59-.39.39-.58.86-.58 1.41zm5.6 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.43.59.56 0 1.03-.2 1.42-.59.39-.39.58-.86.58-1.41 0-.55-.2-1.02-.58-1.41a1.93 1.93 0 0 0-1.42-.59c-.56 0-1.04.2-1.43.59-.39.39-.58.86-.58 1.41z" fill-rule="evenodd"></path></svg>
@@ -100,5 +95,4 @@ $(document).ready(function(){
       }).fail((err) => {
         console.log(err)
       })
-  getPost()
 })

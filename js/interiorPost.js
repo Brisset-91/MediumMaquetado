a098aee54ae42afdc0c86e9ee891c4a9a8d19590
juanmcolorado <jcolorado743@gmail.com>
2022-01-12 21:56:56 -0600

    viewpost complete

--- js/interiorPost.js
+++ js/interiorPost.js
@@ -2,10 +2,22 @@ $(()=>{
     let idPost = location.search.slice(8)
     $.ajax({
         method:'GET',
-        url: `https://medium-7cfcc-default-rtdb.firebaseio.com/.json`
+        url: `https://medium-7cfcc-default-rtdb.firebaseio.com/${idPost}.json`
     }).done((resp)=>{
         console.log(resp)
-        $('.text').text(resp.author)
+        let temp = ''
+        let formFile = resp.formFile                
+        temp += `
+        <div>
+        <img src="${formFile}" class="card-image" alt="...">
+        </div>
+        `
+        $('#imagePost').html(temp)
+        $('.titlePost').text(resp.title)
+        $('.authorPost').text(resp.author)
+        $('.timePost').text(resp.readingTime + " min.")
+        $('.datePost').text(resp.date)
+        $('.textPost').text(resp.postContent)
     })
 
 })
